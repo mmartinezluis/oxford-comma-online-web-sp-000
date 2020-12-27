f37fffd3f8ad4a1c57789fe08bde9d6fe55d0958
Luis Martinez <luis_mmartinez@live.com>
2020-12-27 06:45:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join("and")
+  when 3
+    array.pop.join("and")
+    array.join(",")
+  when >3
+    
+    
+    
+    
 end
