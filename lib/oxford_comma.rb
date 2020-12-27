e09b0d51815a295bc7a50b85853e41e201c85e92
Luis Martinez <luis_mmartinez@live.com>
2020-12-27 07:06:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
+require "pry"
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  when 3
+    last_modified="and "<<array.pop
+    array.push(last_modified)
+    array.join(", ")
+  else
+    last_modified="and "<<array.pop
+    array.push(last_modified)
+    array.join(", ")
+  end
 end
